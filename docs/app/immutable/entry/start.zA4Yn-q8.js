9c3f1bd103a764833fa9b8030a85cdcd7664d353
stitec-nathanneitman <nathan.neitman@sti-tec.com>
2024-12-15 03:16:12 -0500

    SU-04: Services are up for web browser use

---
+++ docs/app/immutable/entry/start.zA4Yn-q8.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.BJMJ_IE-.js";export{t as start};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,50 @@
 <section>
 	<div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8 flex justify-center">
 		<div class="space-y-4 md:space-y-8">
-			
 			<div class="max-w-xl">
 				<h1 class="text-2xl font-semibold text-gray-900 sm:text-4xl">STI-TEC Public Status</h1>
-                <div class="divider divider-neutral"></div>
+				<div class="divider divider-neutral"></div>
 
+				<div role="alert" class="alert alert-success">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="h-6 w-6 shrink-0 stroke-current"
+						fill="none"
+						viewBox="0 0 24 24"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+						/>
+					</svg>
+					<span>
+						You can now safely access your Outlook and Teams <span class="font-bold"
+							>from web browsers</span
+						>, links in the updates feed below.</span
+					>
+				</div>
 				<div role="alert" class="alert alert-warning">
 					<svg
-					  xmlns="http://www.w3.org/2000/svg"
-					  class="h-6 w-6 shrink-0 stroke-current"
-					  fill="none"
-					  viewBox="0 0 24 24">
-					  <path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+						xmlns="http://www.w3.org/2000/svg"
+						class="h-6 w-6 shrink-0 stroke-current"
+						fill="none"
+						viewBox="0 0 24 24"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+						/>
 					</svg>
-					<span class="font-bold">Warning: In anticipation of the service migration, outages are expected for all services from evening of Dec 13th 2024 to evening of Dec 15th 2024</span>
-				  </div>
+					<span>
+						Warning: The majority of the migration work is complete, but expect some minor and
+						intermittent degredation of service as we work to resolve any issues that may come up.</span
+					>
+				</div>
 
-				
 				<h2 class="text-2xl font-semibold text-gray-900 sm:text-2xl mt-4">Services</h2>
 				<div class="overflow-x-auto">
 					<table class="table">
@@ -37,25 +60,41 @@
 							<!-- row 1 -->
 							<tr>
 								<td>Teams</td>
-								<td><div class="badge badge-error">Offline</div></td>
-								<td><div class="badge badge-neutral badge-outline whitespace-nowrap">IT Validation Testing</div></td>
+								<td><div class="badge badge-success">Operational</div></td>
+								<td
+									><div class="badge badge-neutral badge-outline whitespace-nowrap">
+										IT Validation Testing
+									</div></td
+								>
 							</tr>
 							<!-- row 2 -->
 							<tr>
 								<td>Outlook</td>
-								<td><div class="badge badge-error">Offline</div></td>
-								<td><div class="badge badge-neutral badge-outline whitespace-nowrap">IT Validation Testing</div></td>
+								<td><div class="badge badge-success">Operational</div></td>
+								<td
+									><div class="badge badge-neutral badge-outline whitespace-nowrap">
+										IT Validation Testing
+									</div></td
+								>
 							</tr>
 							<!-- row 3 -->
 							<tr>
 								<td>SharePoint</td>
-								<td><div class="badge badge-error">Offline</div></td>
-								<td><div class="badge badge-neutral badge-outline whitespace-nowrap">IT Validation Testing</div></td>
+								<td><div class="badge badge-success">Operational</div></td>
+								<td
+									><div class="badge badge-neutral badge-outline whitespace-nowrap">
+										IT Validation Testing
+									</div></td
+								>
 							</tr>
 							<tr>
 								<td>OneDrive</td>
-								<td><div class="badge badge-error">Offline</div></td>
-								<td><div class="badge badge-neutral badge-outline whitespace-nowrap">IT Validation Testing</div></td>
+								<td><div class="badge badge-success">Operational</div></td>
+								<td
+									><div class="badge badge-neutral badge-outline whitespace-nowrap">
+										IT Validation Testing
+									</div></td
+								>
 							</tr>
 						</tbody>
 					</table>
@@ -117,53 +156,34 @@
 							</svg>
 						</div>
 						<div class="timeline-end timeline-box">
-							<div class="badge badge-primary badge-outline mr-1">Latest</div><div class="badge badge-neutral badge-outline whitespace-nowrap">14 Dec @ 10:00pm EST</div>
-							Migrated services are currently being tested by the IT team and our trusted partners. Please hold and do not utilize the migrated services until testing is complete.
+							<div>
+								<div class="badge badge-primary badge-outline mr-1">Latest</div>
+								<div class="badge badge-neutral badge-outline whitespace-nowrap">
+									15 Dec @ 2:20am EST
+								</div>
+							</div>
+							Services have successfully been migrated and should be accessible for web browser use.
+							The Outlook and Teams workstation and mobile applications have some unexpected behaviour
+							that likely amounts to credential caching that we are currently working to resolve.
+							<div class="divider divider-neutral"></div>
+							<p>Access Outlook and Teams in browser at:</p>
+							<ul>
+								<li class="my-1">
+									<a href="https://outlook.office.com" class="text-blue-500" target="_blank"
+										>https://outlook.office.com</a
+									>
+								</li>
+								<li>
+									<a href="https://teams.microsoft.com" class="text-blue-500" target="_blank"
+										>https://teams.microsoft.com</a
+									>
+								</li>
+							</ul>
+							<div class="divider divider-neutral"></div>
+							<p>Browser troubleshooting tip:</p>
+							<kbd class="kbd">shift</kbd> +
+							<kbd class="kbd">F5</kbd> or try incognito mode.
 						</div>
-						<hr />
-					</li> 
-					<li>
-						<hr />
-						<div class="timeline-middle">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								viewBox="0 0 20 20"
-								fill="currentColor"
-								class="h-5 w-5"
-							>
-								<path
-									fill-rule="evenodd"
-									d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-									clip-rule="evenodd"
-								/>
-							</svg>
-						</div>
-						<div class="timeline-end timeline-box">
-							<div><div class="badge badge-neutral badge-outline whitespace-nowrap">13 Dec @ 9:20pm EST</div></div>
-							Service Migration is in progress. Discontinue use of company Outlook and Teams services until we are officially cut over to our new environment. Additionally, follow transition steps emailed to you if you have not already.
-						</div>
-						<hr />
-					</li> 
-					<li>
-						<hr />
-						<div class="timeline-middle">
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								viewBox="0 0 20 20"
-								fill="currentColor"
-								class="h-5 w-5"
-							>
-								<path
-									fill-rule="evenodd"
-									d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-									clip-rule="evenodd"
-								/>
-							</svg>
-						</div>
-						<div class="timeline-end timeline-box">
-                            <div><div class="badge badge-neutral badge-outline whitespace-nowrap">12 Dec @ 5:00pm EST</div></div>
-                            Final review with technical partners for Friday evening cutover to new hosting services.
-                        </div>
 						<hr />
 					</li>
 					<li>
@@ -183,9 +203,94 @@
 							</svg>
 						</div>
 						<div class="timeline-end timeline-box">
-                            <div><div class="badge badge-neutral badge-outline whitespace-nowrap">11 Dec @ 5:20pm EST</div></div>
-                            Transition instructions email shared with the firm.
-                        </div>
+							<div>
+								<div class="badge badge-neutral badge-outline whitespace-nowrap">
+									14 Dec @ 10:00pm EST
+								</div>
+							</div>
+							Migrated services are currently being tested by the IT team and our trusted partners. Please
+							hold and do not utilize the migrated services until testing is complete.
+						</div>
+						<hr />
+					</li>
+					<li>
+						<hr />
+						<div class="timeline-middle">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 20 20"
+								fill="currentColor"
+								class="h-5 w-5"
+							>
+								<path
+									fill-rule="evenodd"
+									d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+									clip-rule="evenodd"
+								/>
+							</svg>
+						</div>
+						<div class="timeline-end timeline-box">
+							<div>
+								<div class="badge badge-neutral badge-outline whitespace-nowrap">
+									13 Dec @ 9:20pm EST
+								</div>
+							</div>
+							Service Migration is in progress. Discontinue use of company Outlook and Teams services
+							until we are officially cut over to our new environment. Additionally, follow transition
+							steps emailed to you if you have not already.
+						</div>
+						<hr />
+					</li>
+					<li>
+						<hr />
+						<div class="timeline-middle">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 20 20"
+								fill="currentColor"
+								class="h-5 w-5"
+							>
+								<path
+									fill-rule="evenodd"
+									d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+									clip-rule="evenodd"
+								/>
+							</svg>
+						</div>
+						<div class="timeline-end timeline-box">
+							<div>
+								<div class="badge badge-neutral badge-outline whitespace-nowrap">
+									12 Dec @ 5:00pm EST
+								</div>
+							</div>
+							Final review with technical partners for Friday evening cutover to new hosting services.
+						</div>
+						<hr />
+					</li>
+					<li>
+						<hr />
+						<div class="timeline-middle">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 20 20"
+								fill="currentColor"
+								class="h-5 w-5"
+							>
+								<path
+									fill-rule="evenodd"
+									d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+									clip-rule="evenodd"
+								/>
+							</svg>
+						</div>
+						<div class="timeline-end timeline-box">
+							<div>
+								<div class="badge badge-neutral badge-outline whitespace-nowrap">
+									11 Dec @ 5:20pm EST
+								</div>
+							</div>
+							Transition instructions email shared with the firm.
+						</div>
 					</li>
 				</ul>
 			</div>
