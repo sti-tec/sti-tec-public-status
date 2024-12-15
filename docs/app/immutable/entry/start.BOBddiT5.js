550bd6a49aba0c36db3b592db7d22494b621bd63
stitec-nathanneitman <nathan.neitman@sti-tec.com>
2024-12-15 17:17:06 -0500

    SU-05: VDIs and domain accounts are good for use

---
+++ docs/app/immutable/entry/start.BOBddiT5.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.L4wrMGHv.js";export{t as start};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -159,6 +159,32 @@
 							<div>
 								<div class="badge badge-primary badge-outline mr-1">Latest</div>
 								<div class="badge badge-neutral badge-outline whitespace-nowrap">
+									15 Dec @ 5:00pm EST
+								</div>
+							</div>
+							Users are now safe to login and begin using their domain accounts on workstations, laptops, and VDI instances.
+						</div>
+						<hr />
+					</li>
+					<li>
+						<hr />
+						<div class="timeline-middle">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 20 20"
+								fill="currentColor"
+								class="h-5 w-5"
+							>
+								<path
+									fill-rule="evenodd"
+									d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+									clip-rule="evenodd"
+								/>
+							</svg>
+						</div>
+						<div class="timeline-end timeline-box">
+							<div>
+								<div class="badge badge-neutral badge-outline whitespace-nowrap">
 									15 Dec @ 2:20am EST
 								</div>
 							</div>
